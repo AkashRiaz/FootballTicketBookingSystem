9555424530b8e420aceac2017d18ee631698be13
Md Akashuzzaman Riaz <akashriaz05@gmail.com>
2026-06-14 01:07:48 +0600

    database table created succefully

--- QUERY.sql
+++ QUERY.sql
@@ -32,3 +32,53 @@ CREATE TYPE payment_status AS ENUM (
     'Cancelled',
     'Refunded'
 );
+
+
+-- =========================================================================
+-- 1. CREATE USERS TABLE
+-- =========================================================================
+
+
+CREATE TABLE Users (
+    user_id SERIAL PRIMARY KEY,
+    full_name VARCHAR(100),
+    email VARCHAR(150) UNIQUE,
+    role user_role NOT NULL,
+    phone_number VARCHAR(20)
+);
+
+-- =========================================================================
+-- 2. CREATE MATCHES TABLE
+-- =========================================================================
+CREATE TABLE Matches (
+    match_id SERIAL PRIMARY KEY,
+    fixture VARCHAR(150),
+    tournament_category VARCHAR(100),
+    base_ticket_price DECIMAL(10, 2),
+    match_status match_status NOT NULL,
+    CONSTRAINT chk_ticket_price
+        CHECK (base_ticket_price >= 0)
+);
+
+-- =========================================================================
+-- 3. CREATE BOOKINGS TABLE
+-- =========================================================================
+CREATE TABLE Bookings (
+    booking_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    match_id INT NOT NULL,
+    seat_number VARCHAR(20),
+    payment_status payment_status,
+    total_cost DECIMAL(10, 2),
+
+    CONSTRAINT fk_booking_user
+        FOREIGN KEY (user_id)
+        REFERENCES Users(user_id),
+
+    CONSTRAINT fk_booking_match
+        FOREIGN KEY (match_id)
+        REFERENCES Matches(match_id),
+
+    CONSTRAINT chk_total_cost
+        CHECK (total_cost >= 0)
+);
